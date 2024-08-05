6d74e2f5a3e924dcd5c0516b25e12fc19ed6e35b
Adeel Sohail <asohail_17@hotmail.com>
2024-08-05 19:56:51 -0400

    first project

--- desktopCleaner.py
+++ desktopCleaner.py
@@ -1,1 +1,18 @@
-print("Hi I'm adeel")
+import os
+import shutil
+
+lis = []
+
+sourcedir = r'C:\users\asoha\desktop\\'
+destinationdir = r'C:\Users\asoha\cleanup'
+
+
+if not os.path.exists(destinationdir): 
+    os.makedirs(destinationdir)
+
+lis = os.listdir(sourcedir)
+for x in lis:
+    print(sourcedir + x)
+    if x == __file__:
+        continue
+    shutil.move((sourcedir + x),destinationdir)
